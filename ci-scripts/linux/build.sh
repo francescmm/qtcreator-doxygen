3abf1e70ff9aa389079fa6d667cd64835421db22
Francesc M <francesc.martinez@king.com>
2020-08-06 09:13:58 +0200

    Reverting wget name change

--- ci-scripts/linux/build.sh
+++ ci-scripts/linux/build.sh
@@ -1,8 +1,8 @@
-wget qt5.zip https://github.com/francescmm/ci-utils/releases/download/qt/qt5.zip;
+wget -O qt5.zip https://github.com/francescmm/ci-utils/releases/download/qt/qt5.zip;
 unzip -qq qt5.zip;
-wget qtcreator-src.zip https://github.com/francescmm/ci-utils/releases/download/qt/qt-creator-opensource-src-${QTC_VERSION}.0.zip;
+wget -O qtcreator-src.zip https://github.com/francescmm/ci-utils/releases/download/qt/qt-creator-opensource-src-${QTC_VERSION}.0.zip;
 unzip -qq qtcreator-src.zip
-wget qtcreator-bin.zip https://github.com/francescmm/ci-utils/releases/download/qt/qt-creator-bin-linux.zip;
+wget -O qtcreator-bin.zip https://github.com/francescmm/ci-utils/releases/download/qt/qt-creator-bin-linux.zip;
 unzip -qq qtcreator-bin.zip
 export QTDIR=$PWD/qt5
 export QT_CREATOR=$PWD/Tools/QtCreator/bin
